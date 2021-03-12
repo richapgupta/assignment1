081c29ce422c36e3504a45af768f88c10aebf81f
Richa Gupta <rkg3387@gmail.com>
2021-03-12 16:03:14 -0500

    added graphQLFetch function

--- public/ims.js
+++ public/ims.js
@@ -1,3 +1,32 @@
+const productTableHeadings = ['Product Name', 'Price', 'Category', 'Image'];
+const NO_DATA_AVAILABLE = 'No Data Available';
+
+function MsgRow(props) {
+  const msg = props.msg;
+  return /*#__PURE__*/React.createElement("tr", {
+    className: "text-center"
+  }, /*#__PURE__*/React.createElement("td", {
+    colSpan: "4"
+  }, msg));
+}
+
+function ErrorMsgAlert(props) {
+  const msgs = props.errorMsg;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
+    className: "alert alert-danger alert-dismissible fade show",
+    role: "alert"
+  }, /*#__PURE__*/React.createElement("strong", null, /*#__PURE__*/React.createElement("ul", null, msgs.map(msg => {
+    return /*#__PURE__*/React.createElement("li", null, msg.extensions.code, " - ", msg.message, " ");
+  }))), /*#__PURE__*/React.createElement("button", {
+    type: "button",
+    className: "close",
+    "data-dismiss": "alert",
+    "aria-label": "Close"
+  }, /*#__PURE__*/React.createElement("span", {
+    "aria-hidden": "true"
+  }, "\xD7"))));
+}
+
 function ProductRow(props) {
   const product = props.product;
   return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, product.name), /*#__PURE__*/React.createElement("td", null, "$", product.price), /*#__PURE__*/React.createElement("td", null, product.category), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("a", {
@@ -7,13 +36,23 @@ function ProductRow(props) {
 }
 
 function ProductTable(props) {
-  const productRows = props.products.map(product => /*#__PURE__*/React.createElement(ProductRow, {
+  const {
+    headings,
+    products
+  } = props;
+  const productRows = products.map(product => /*#__PURE__*/React.createElement(ProductRow, {
     key: product.id,
     product: product
   }));
-  return /*#__PURE__*/React.createElement("table", {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("table", {
     className: 'table' + ' table-striped' + ' table-hover'
-  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Product Name"), /*#__PURE__*/React.createElement("th", null, "Price"), /*#__PURE__*/React.createElement("th", null, "Category"), /*#__PURE__*/React.createElement("th", null, "Image"))), /*#__PURE__*/React.createElement("tbody", null, productRows));
+  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, headings.map((heading, index) => {
+    return /*#__PURE__*/React.createElement("th", {
+      key: index
+    }, heading);
+  }))), /*#__PURE__*/React.createElement("tbody", null, products.length > 0 ? productRows : /*#__PURE__*/React.createElement(MsgRow, {
+    msg: 'No Data'
+  }))));
 }
 
 class ProductAdd extends React.Component {
@@ -24,18 +63,23 @@ class ProductAdd extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const form = document.forms.productAdd;
+    const {
+      name,
+      price,
+      category,
+      image
+    } = document.forms.productAdd;
     const product = {
-      category: form.pcategory.value,
-      price: form.pprice.value,
-      name: form.pname.value,
-      image: form.pimage.value
+      category: category.value,
+      price: price.value,
+      name: name.value,
+      image: image.value
     };
     this.props.createProduct(product);
-    form.pcategory.value = "";
-    form.pprice.value = "";
-    form.pname.value = "";
-    form.pimage.value = "";
+    category.value = "";
+    price.value = "";
+    name.value = "";
+    image.value = "";
   }
 
   render() {
@@ -47,27 +91,37 @@ class ProductAdd extends React.Component {
       className: "form-group"
     }, /*#__PURE__*/React.createElement("label", null, "Category"), /*#__PURE__*/React.createElement("select", {
       className: "form-control",
-      name: "pcategory"
-    }, /*#__PURE__*/React.createElement("option", null, "T-shirt"), /*#__PURE__*/React.createElement("option", null, "Jeans"), /*#__PURE__*/React.createElement("option", null, "Jacket"), /*#__PURE__*/React.createElement("option", null, "Sweater"), /*#__PURE__*/React.createElement("option", null, "Accessories"))), /*#__PURE__*/React.createElement("div", {
+      name: "category"
+    }, /*#__PURE__*/React.createElement("option", {
+      value: "Shirts"
+    }, "Shirts"), /*#__PURE__*/React.createElement("option", {
+      value: "Jeans"
+    }, "Jeans"), /*#__PURE__*/React.createElement("option", {
+      value: "Jackets"
+    }, "Jacket"), /*#__PURE__*/React.createElement("option", {
+      value: "Sweaters"
+    }, "Sweater"), /*#__PURE__*/React.createElement("option", {
+      value: "Accessories"
+    }, "Accessories"))), /*#__PURE__*/React.createElement("div", {
       className: "form-group"
     }, /*#__PURE__*/React.createElement("label", null, "Price Per Unit"), /*#__PURE__*/React.createElement("input", {
       className: "form-control",
       type: "text",
-      name: "pprice",
+      name: "price",
       placeholder: "$"
     })), /*#__PURE__*/React.createElement("div", {
       className: "form-group"
     }, /*#__PURE__*/React.createElement("label", null, "Product Name "), /*#__PURE__*/React.createElement("input", {
       className: "form-control",
       type: "text",
-      name: "pname",
+      name: "name",
       placeholder: "Name"
     })), /*#__PURE__*/React.createElement("div", {
       className: "form-group"
     }, /*#__PURE__*/React.createElement("label", null, " Image Url"), /*#__PURE__*/React.createElement("input", {
       className: "form-control",
       type: "text",
-      name: "pimage",
+      name: "image",
       placeholder: "Image Url"
     })), /*#__PURE__*/React.createElement("div", {
       className: "form-group" + " align-self-end"
@@ -78,23 +132,93 @@ class ProductAdd extends React.Component {
 
 }
 
+async function graphQLFetch(query, variables = {}, setErrorState = () => {}) {
+  try {
+    const response = await fetch('/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        query,
+        variables
+      })
+    });
+    const result = await response.json();
+
+    if (result.errors) {
+      setErrorState(result.errors);
+      return result.errors;
+    }
+
+    console.log('Here also - ', result);
+    return result.data;
+  } catch (e) {
+    console.log('inside.catch.error', e);
+  }
+}
+
 class ProductList extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: []
+      products: [],
+      errors: []
     };
     this.createProduct = this.createProduct.bind(this);
+    this.setErrorState = this.setErrorState.bind(this);
   }
 
-  createProduct(product) {
-    product.id = this.state.products.length + 1;
-    const newProductList = this.state.products.slice();
-    newProductList.push(product);
+  componentDidMount() {
+    this.loadData();
+  }
+
+  setErrorState(errorMsg) {
     this.setState({
-      products: newProductList
+      errors: errorMsg
     });
-    console.log('newProductList', newProductList);
+  }
+
+  async loadData() {
+    const query = `
+        query {
+            productList {
+                id
+                name
+                category
+                price
+                image
+            }
+        }`;
+    const data = await graphQLFetch(query, this.setErrorState);
+
+    if (data) {
+      this.setState({
+        products: data.productList
+      });
+    }
+  }
+
+  async createProduct(product) {
+    const query = `
+       mutation addProduct($product: ProductInputs!) {
+           addProduct(product: $product) {
+               id
+           }
+       }`;
+    const data = await graphQLFetch(query, {
+      product
+    }, this.setErrorState);
+    console.log('Data after graphqlfetch - ', data);
+
+    if (data) {
+      this.loadData();
+    } // product.id = this.state.products.length + 1;
+    // const newProductList = this.state.products.slice();
+    // newProductList.push(product);
+    // this.setState({products:newProductList});
+    // console.log('newProductList', newProductList)
+
   }
 
   render() {
@@ -102,8 +226,12 @@ class ProductList extends React.Component {
       className: 'container'
     }, /*#__PURE__*/React.createElement("h1", {
       className: 'text-center m-3 mt-5'
-    }, " My Company Inventory"), /*#__PURE__*/React.createElement("p", null, "Showing ", this.state.products.length, " available products"), /*#__PURE__*/React.createElement(ProductTable, {
-      products: this.state.products
+    }, " My Company Inventory"), this.state.errors?.length ? /*#__PURE__*/React.createElement(ErrorMsgAlert, {
+      errorMsg: this.state.errors
+    }) : '', /*#__PURE__*/React.createElement("p", null, "Showing ", this.state.products.length, " available products"), /*#__PURE__*/React.createElement(ProductTable, {
+      headings: productTableHeadings,
+      products: this.state.products,
+      errors: this.state.errors
     }), /*#__PURE__*/React.createElement("hr", {
       className: 'mt-5'
     }), /*#__PURE__*/React.createElement(ProductAdd, {
--- src/ims.jsx
+++ src/ims.jsx
@@ -1,3 +1,31 @@
+const productTableHeadings = ['Product Name', 'Price', 'Category', 'Image'];
+const NO_DATA_AVAILABLE = 'No Data Available';
+
+function MsgRow(props) {
+    const msg = props.msg;
+    return (
+        <tr className="text-center"><td colSpan="4">{msg}</td></tr>
+    )
+}
+
+function ErrorMsgAlert(props) {
+    const msgs = props.errorMsg;
+    return (
+        <React.Fragment>
+            <div className="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>
+               <ul>
+                { msgs.map((msg)=> {  return <li>{msg.extensions.code} - {msg.message} </li> })
+                }
+                </ul>
+            </strong>
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+        </React.Fragment>
+    )
+}
 
 function ProductRow(props) {
     const product = props.product;
@@ -8,26 +36,30 @@ function ProductRow(props) {
             <td>{product.category}</td>
             <td><a href={product.image} target='_blank'>View</a></td>
         </tr>
-    )
+    );
 }
 
 
 function ProductTable(props) {
-    const productRows = props.products.map( product => <ProductRow key={product.id} product={product}/>)
+    const {headings, products} = props;
+    const productRows = products.map( product => <ProductRow key={product.id} product={product}/>)
     return (
-        <table className={'table' + ' table-striped' + ' table-hover'}>
+<React.Fragment>
+            <table className={'table' + ' table-striped' + ' table-hover'}>
             <thead>
             <tr>
-                <th>Product Name</th>
-                <th>Price</th>
-                <th>Category</th>
-                <th>Image</th>
+                {
+                    headings.map((heading, index)=> {
+                    return <th key={index}>{heading}</th>
+                    })
+                }
             </tr>
             </thead>
             <tbody>
-                {productRows}
+                { products.length > 0 ? productRows : (<MsgRow msg={'No Data'} />)}
             </tbody>
         </table>
+</React.Fragment>
     )
 }
 
@@ -38,45 +70,45 @@ class ProductAdd extends React.Component {
     }
     handleSubmit(e) {
         e.preventDefault();
-        const form = document.forms.productAdd;
+        const { name, price, category, image}  = document.forms.productAdd;
         const product = {
-            category: form.pcategory.value,
-            price:form.pprice.value,
-            name:form.pname.value,
-            image:form.pimage.value,
+            category: category.value,
+            price:price.value,
+            name:name.value,
+            image:image.value,
         }
 
         this.props.createProduct(product);
 
-        form.pcategory.value = "";
-        form.pprice.value = "";
-        form.pname.value = "";
-        form.pimage.value = "";
+        category.value = "";
+        price.value = "";
+        name.value = "";
+        image.value = "";
     }
     render() {
     return (
         <form className={'d-flex' + ' justify-content-between'} name="productAdd" onSubmit={this.handleSubmit}>
            <div className={"form-group"}>
             <label>Category</label>
-            <select className={"form-control"} name="pcategory">
-                <option>T-shirt</option>
-                <option>Jeans</option>
-                <option>Jacket</option>
-                <option>Sweater</option>
-                <option>Accessories</option>
+            <select className={"form-control"} name="category">
+                <option value="Shirts">Shirts</option>
+                <option value="Jeans">Jeans</option>
+                <option value="Jackets">Jacket</option>
+                <option value="Sweaters">Sweater</option>
+                <option value="Accessories">Accessories</option>
             </select>
             </div>
             <div className={"form-group"}>
             <label>Price Per Unit</label>
-            <input className={"form-control"} type="text" name="pprice" placeholder="$"/>
+            <input className={"form-control"} type="text" name="price" placeholder="$"/>
             </div>
             <div className={"form-group"}>
             <label>Product Name </label>
-            <input className={"form-control"} type="text" name="pname" placeholder="Name"/>
+            <input className={"form-control"} type="text" name="name" placeholder="Name"/>
             </div>
             <div className={"form-group"}>
             <label> Image Url</label>
-            <input className={"form-control"} type="text" name="pimage" placeholder="Image Url"/>
+            <input className={"form-control"} type="text" name="image" placeholder="Image Url"/>
             </div>
             <div className={"form-group"+ " align-self-end"}>
             <button className={'btn ' + ' btn-primary'}>Add Product</button>
@@ -86,30 +118,91 @@ class ProductAdd extends React.Component {
     }
 }
 
+async function graphQLFetch(query, variables = {}, setErrorState = () => {}) {
+    try {
+        const response = await fetch('/graphql', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ query, variables })
+        });
+        const result = await response.json();
+
+        if(result.errors) {
+            setErrorState(result.errors);
+            return result.errors;
+        }
+        console.log('Here also - ', result);
+        return result.data;
+    } catch(e) {
+        console.log('inside.catch.error',e);
+
+    }
+}
+
 class ProductList extends React.Component {
     constructor() {
         super();
-        this.state = {products: []};
+        this.state = {products: [], errors:[]};
         this.createProduct = this.createProduct.bind(this);
+        this.setErrorState = this.setErrorState.bind(this);
     }
 
-    createProduct(product) {
-        product.id = this.state.products.length + 1;
-        const newProductList = this.state.products.slice();
-        newProductList.push(product);
-        this.setState({products:newProductList});
-        console.log('newProductList', newProductList)
+    componentDidMount() {
+        this.loadData();
+    }
+
+    setErrorState(errorMsg) {
+        this.setState({errors:errorMsg});
+    }
+
+    async loadData() {
+        const query = `
+        query {
+            productList {
+                id
+                name
+                category
+                price
+                image
+            }
+        }`;
+
+        const data = await graphQLFetch(query, this.setErrorState);
+        if(data) {
+            this.setState({products: data.productList})
+        }
+    }
+
+   async createProduct(product) {
+       const query = `
+       mutation addProduct($product: ProductInputs!) {
+           addProduct(product: $product) {
+               id
+           }
+       }`;
+
+       const data = await graphQLFetch(query, {product}, this.setErrorState);
+       console.log('Data after graphqlfetch - ', data);
+       if(data) {
+           this.loadData();
+       }
+        // product.id = this.state.products.length + 1;
+        // const newProductList = this.state.products.slice();
+        // newProductList.push(product);
+        // this.setState({products:newProductList});
+        // console.log('newProductList', newProductList)
     }
 
     render() {
         return (
              <div className={'container'}>
                 <h1 className={'text-center m-3 mt-5'}> My Company Inventory</h1>
+                { this.state.errors?.length ? <ErrorMsgAlert errorMsg={this.state.errors} /> : ''}
                 <p>Showing {this.state.products.length} available products</p>
-            <ProductTable products= {this.state.products}/>
-            <hr className={'mt-5'}/>
-            <ProductAdd createProduct={this.createProduct} />
-            <hr/>
+                <ProductTable headings={productTableHeadings} products= {this.state.products} errors={this.state.errors}/>
+                <hr className={'mt-5'}/>
+                <ProductAdd createProduct={this.createProduct} />
+                <hr/>
             </div>
         )
     }
